78779816f00933e6b4d507158881e557c1fc0289
jeessh <jesshuang5733@gmail.com>
2023-01-23 22:01:15 -0500

    Updated Resume

--- src/components/1-navbar/index.js
+++ src/components/1-navbar/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 import './navbar.css';
 import { ImProfile } from 'react-icons/im'
 import {Link as LinkS, animateScroll} from 'react-scroll'
-import pdf from '../../documents/FirstYearResume.pdf'
+import pdf from '../../documents/summer_resume_2023.pdf'
 
 
 const Navbar = () => {
